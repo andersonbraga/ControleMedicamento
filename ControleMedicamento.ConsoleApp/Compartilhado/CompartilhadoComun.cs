9f2f42558b89c864179f214fb406f4662aacb271
Anderson Braga Oliveira <anderson_braga_oliveira@hotmail.com>
2023-04-19 14:58:33 -0300

    Repositorio Paciente implementado

---
+++ ControleMedicamento.ConsoleApp/Compartilhado/CompartilhadoComun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleMedicamento.ConsoleApp.Compartilhado
+{
+    internal class CompartilhadoComun
+    {
+        public List<object> listaObj { get; set; } = new List<object>();
+    }
+}
--- ControleMedicamento.ConsoleApp/ModuloFuncionario/RepositorioFuncionario.cs
+++ ControleMedicamento.ConsoleApp/ModuloFuncionario/RepositorioFuncionario.cs
@@ -8,5 +8,6 @@ namespace ControleMedicamento.ConsoleApp.ModuloFuncionario
 {
     internal class RepositorioFuncionario
     {
+
     }
 }
