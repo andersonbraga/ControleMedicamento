a19ff052f1f31d8c1ca05d1c657a003263f83cb6
Anderson Braga Oliveira <anderson_braga_oliveira@hotmail.com>
2023-04-20 19:11:49 -0300

    Merge Medicamento

--- ControleMedicamento.ConsoleApp/ModuloPaciente/RepositorioPaciente.cs
+++ ControleMedicamento.ConsoleApp/ModuloPaciente/RepositorioPaciente.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace ControleMedicamento.ConsoleApp.ModuloPaciente
 {
-    internal class RepositorioPaciente
+    internal class RepositorioPaciente : CompartilhadoComun
     {
+                          
+        public void CadastrarPaciente(Paciente paciente)
+        {
+            Adicionar(paciente);
+            paciente.Id = listaObj.Count;
+        }
+
+        public virtual Entidade Editar(Paciente pacienteAtualizado, int id)
+        {
+            Paciente paciente = (Paciente)SelecionarPorId(id);
+
+            paciente.Nome = pacienteAtualizado.Nome;
+            paciente.Cpf = pacienteAtualizado.Cpf;
+            paciente.Telefone = pacienteAtualizado.Telefone;
+            paciente.CartaoSus = pacienteAtualizado.CartaoSus;
+            paciente.Endereco = pacienteAtualizado.Endereco;
+
+            return paciente;
+        }
+   
+
+
     }
 }
