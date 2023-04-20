81aefd2be4fce12dc41f2eef226854549a3757f5
Anderson Braga Oliveira <anderson_braga_oliveira@hotmail.com>
2023-04-20 20:08:24 -0300

    Merge

---
+++ ControleMedicamento.ConsoleApp/Compartilhado/CompartilhadoComun.cs
@@ -0,0 +1,52 @@
+﻿using ControleMedicamento.ConsoleApp.ModuloPaciente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleMedicamento.ConsoleApp.Compartilhado
+{
+    internal class CompartilhadoComun
+    {
+
+        public List<object> listaObj { get; set; } = new List<object>();
+
+        public void Adicionar(object item) => listaObj.Add(item);
+
+        public List<object> ListarTodos() => listaObj;
+
+        public Entidade SelecionarPorId(int id)
+        {
+            Entidade entidade = null;
+
+            foreach (Entidade item in listaObj)
+            {
+                if (item.Id == id)
+                {
+                    entidade = item;
+                    break;
+                }
+            }
+            return entidade;
+
+        }
+
+        //############# Implementar Por Ultimo ############
+
+        //public virtual Entidade Editar(Paciente pacienteAtualizado, int id)
+        //{
+        //    Paciente paciente = (Paciente)SelecionarPorId(id);
+
+        //    paciente.Nome = pacienteAtualizado.Nome;
+        //    paciente.Cpf = pacienteAtualizado.Cpf;
+        //    paciente.Telefone = pacienteAtualizado.Telefone;
+        //    paciente.CartaoSus = pacienteAtualizado.CartaoSus;
+        //    paciente.Endereco = pacienteAtualizado.Endereco;
+
+        //    return paciente;
+        //}
+
+
+    }
+}
--- ControleMedicamento.ConsoleApp/ModuloMedicamento/RepositorioMedicamento.cs
+++ ControleMedicamento.ConsoleApp/ModuloMedicamento/RepositorioMedicamento.cs
@@ -8,5 +8,26 @@ namespace ControleMedicamento.ConsoleApp.ModuloMedicamentos
 {
     internal class RepositorioMedicamento
     {
+        //public void CadastrarMedicamento(Medicamento medicamento) => Adicionar(medicamento);
+        public void CadastrarMedicamento(Medicamento medicamento)
+        {
+            Adicionar(medicamento);
+            medicamento.Id = listaObj.Count;
+        }
+
+        public virtual Entidade Editar(Medicamento medicamentoAtualizado, int id)
+        {
+            Medicamento medicamento = (Medicamento)SelecionarPorId(id);
+
+            medicamento.Nome = medicamentoAtualizado.Nome;
+            medicamento.QuantidadeLimite = medicamentoAtualizado.QuantidadeLimite;
+            medicamento.QuantidadeDisponivel = medicamentoAtualizado.QuantidadeDisponivel;
+            medicamento.Descricao = medicamentoAtualizado.Descricao;
+            medicamento.HistoricoRequisicoes = medicamentoAtualizado.HistoricoRequisicoes;
+            medicamento.Fornecedor = medicamentoAtualizado.Fornecedor;
+
+            return medicamento;
+        }
+
     }
 }
